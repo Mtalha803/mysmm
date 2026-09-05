3eaac19294de3e1602b23896297eae0d3ab431a1
Mtalha803 <mtalhasarwar804@gmail.com>
2026-09-05 19:49:35 +0500

    Added CORS support

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+
+// 🔥 CORS Enable Karein
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 
 app.post('/campaigns', async (req, res) => {
